0745fe03686dc6a922947b6cc8e75feceddaebc5
Walter <preparationh67@gmail.com>
2012-04-23 15:32:08 -0500

    Added Google Books files

---
+++ GoogleBooks.php
@@ -0,0 +1,6 @@
+<?php
+
+require("./GoogleBooksConf.php");
+
+?>
+Hello World
---
+++ GoogleBooksConf.php
@@ -0,0 +1,6 @@
+<?php
+define("GB_API_KEY_FILE", "./d992b3cf98f6c5aa6812d831371ab6dd88201cb6-privatekey.p12");
+define()
+
+
+?>
