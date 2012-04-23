2f8bae36b8a3b083abbe9bbff1cf429efaab7baf
Walter <preparationh67@gmail.com>
2012-04-23 15:46:11 -0500

    Added GoogleBooksConf.php to ignore file since it now contains sensitive info, created default config to repo use

--- GoogleBooksConf.php
+++ GoogleBooksConf.php
@@ -1,6 +1,13 @@
 <?php
+//OAuth Service account with private key file and password
 define("GB_API_KEY_FILE", "./d992b3cf98f6c5aa6812d831371ab6dd88201cb6-privatekey.p12");
-define()
+define("GB_API_SERVICE_ACCOUNT", "1013677779152@developer.gserviceaccount.com");
+define("GB_API_KEY_PASSWD", "notasecret");
+//Not sure if usefull
+define("GB_API_PUB_KEY_FP", "d992b3cf98f6c5aa6812d831371ab6dd88201cb6");
+
+//API key for simple auth
+define("GB_API_BASIC_KEY", "AIzaSyCeJe48H_2YUzMBnnMqG0eP_l8aNEVvLrg");
 
 
 ?>
