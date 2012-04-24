e193418004396f478fdc439b763a3e05f3bc725e
Walter <preparationh67@gmail.com>
2012-04-23 19:01:28 -0500

    Added additional config info

--- GoogleBooksConf.php
+++ GoogleBooksConf.php
@@ -1,4 +1,8 @@
 <?php
+//Define bookshelf params
+define("GB_API_BOOKSHELF_NAME", "ACM");
+define("GB_API_BOOKSHELF_UID", "1001");
+
 //OAuth Service account with private key file and password
 define("GB_API_KEY_FILE", "./d992b3cf98f6c5aa6812d831371ab6dd88201cb6-privatekey.p12");
 define("GB_API_SERVICE_ACCOUNT", "1013677779152@developer.gserviceaccount.com");
@@ -8,6 +12,6 @@ define("GB_API_PUB_KEY_FP", "d992b3cf98f6c5aa6812d831371ab6dd88201cb6");
 
 //API key for simple auth
 define("GB_API_BASIC_KEY", "AIzaSyCeJe48H_2YUzMBnnMqG0eP_l8aNEVvLrg");
-
-
+//UserId, may be used
+define("GB_API_USER_UID", "101761297033054277699");
 ?>
