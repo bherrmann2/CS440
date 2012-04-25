e28193bd6ba1f8991c441b97a85320ae8579a97d
Walter <preparationh67@gmail.com>
2012-04-25 10:20:24 -0500

    Rewrote as object

--- GoogleBooks.php
+++ GoogleBooks.php
@@ -13,88 +13,90 @@ require_once("./Book.php");
 
 //add Function to add book to google bookself
 //takes a book object as a param
-function add_google_book($pBookObject)
-{
-	$client = new apiClient();
-	$client->setApplicationName(GB_API_APP_NAME);
-	
-	session_start();
-	//Set cached access token
-	if (isset($_SESSION['gb_api_token']))
-	{
-		$client->setAccessToken($_SESSION['gb_api_token']);
-	}
-
-	//Load key in PKCS 12 format
-	$gb_key = file_get_contents(GB_API_KEY_FILE);
-	$client->setAssertionCredentials(new apiAssertionCredentials(GB_API_SERVICE_ACCOUNT_EMAIL, array('https://www.googleapis.com/auth/books'), $gb_key));
-	$client->setClientId(GB_API_CLIENT_ID);
-
-	$service = new apiBooksService($client);
-	$mylib = $service->mylibrary_bookshelves;
-	//Book code goes here
-	#$shelf2 = $mylib->get(GB_API_BOOKSHELF_UID, array());
-	#echo $shelf2->getVolumeCount();
-	#echo $mylib->addVolume(2, 'HFjLm2BauZ8C', array());
-	#if(!$mylib->addVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array()))
-	$mylib->addVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array());
-	
-	//Update access token
-	if($client->getAccessToken())
-	{
-		$_SESSION['gb_api_token'] = $client->getAccessToken();
-	}
-
-	return 1;
-}
-
-function search_google_books($pISBN)
+class GoogleBooks
 {
 
-}
-
-function find_google_book($pISBN, $pAuthor = array(), $pTitle, $pKeywords = array())
-{
-
-}
-
-function remove_google_book($pBookObject)
-{
-	
-	$client = new apiClient();
-	$client->setApplicationName(GB_API_APP_NAME);
-	
-	session_start();
-	//Set cached access token
-	if (isset($_SESSION['gb_api_token']))
+	public function add_google_book($pBookObject)
 	{
-		$client->setAccessToken($_SESSION['gb_api_token']);
+		$client = new apiClient();
+		$client->setApplicationName(GB_API_APP_NAME);
+
+		session_start();
+		//Set cached access token
+		if (isset($_SESSION['gb_api_token']))
+		{
+			$client->setAccessToken($_SESSION['gb_api_token']);
+		}
+
+		//Load key in PKCS 12 format
+		$gb_key = file_get_contents(GB_API_KEY_FILE);
+		$client->setAssertionCredentials(new apiAssertionCredentials(GB_API_SERVICE_ACCOUNT_EMAIL, array('https://www.googleapis.com/auth/books'), $gb_key));
+		$client->setClientId(GB_API_CLIENT_ID);
+
+		$service = new apiBooksService($client);
+		$mylib = $service->mylibrary_bookshelves;
+		//Book code goes here
+		#$shelf2 = $mylib->get(GB_API_BOOKSHELF_UID, array());
+		#echo $shelf2->getVolumeCount();
+		#echo $mylib->addVolume(2, 'HFjLm2BauZ8C', array());
+		#if(!$mylib->addVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array()))
+		$mylib->addVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array());
+
+		//Update access token
+		if($client->getAccessToken())
+		{
+			$_SESSION['gb_api_token'] = $client->getAccessToken();
+		}
+
+		return 1;
 	}
 
-	//Load key in PKCS 12 format
-	$gb_key = file_get_contents(GB_API_KEY_FILE);
-	$client->setAssertionCredentials(new apiAssertionCredentials(GB_API_SERVICE_ACCOUNT_EMAIL, array('https://www.googleapis.com/auth/books'), $gb_key));
-	$client->setClientId(GB_API_CLIENT_ID);
-
-	$service = new apiBooksService($client);
-	$mylib = $service->mylibrary_bookshelves;
-	//Book code goes here
-//	$shelf2 = $mylib->get(GB_API_BOOKSHELF_UID, array());
-//	echo $shelf2->getVolumeCount();
-	#echo $mylib->addVolume(2, 'HFjLm2BauZ8C', array());
-	#if(!$mylib->addVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array()))
-	$mylib->removeVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array());
-	
-	//Update access token
-	if($client->getAccessToken())
+	public function search_google_books($pISBN)
 	{
-		$_SESSION['gb_api_token'] = $client->getAccessToken();
+
 	}
 
-	return 1;
-}
+	public function find_google_book($pISBN, $pAuthor = array(), $pTitle, $pKeywords = array())
+	{
 
+	}
+
+	public function remove_google_book($pBookObject)
+	{
+
+		$client = new apiClient();
+		$client->setApplicationName(GB_API_APP_NAME);
+
+		session_start();
+		//Set cached access token
+		if (isset($_SESSION['gb_api_token']))
+		{
+			$client->setAccessToken($_SESSION['gb_api_token']);
+		}
+
+		//Load key in PKCS 12 format
+		$gb_key = file_get_contents(GB_API_KEY_FILE);
+		$client->setAssertionCredentials(new apiAssertionCredentials(GB_API_SERVICE_ACCOUNT_EMAIL, array('https://www.googleapis.com/auth/books'), $gb_key));
+		$client->setClientId(GB_API_CLIENT_ID);
+
+		$service = new apiBooksService($client);
+		$mylib = $service->mylibrary_bookshelves;
+		//Book code goes here
+		//	$shelf2 = $mylib->get(GB_API_BOOKSHELF_UID, array());
+		//	echo $shelf2->getVolumeCount();
+		#echo $mylib->addVolume(2, 'HFjLm2BauZ8C', array());
+		#if(!$mylib->addVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array()))
+		$mylib->removeVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array());
+
+		//Update access token
+		if($client->getAccessToken())
+		{
+			$_SESSION['gb_api_token'] = $client->getAccessToken();
+		}
+
+		return 1;
+	}
+}
 //add_google_book(NULL);
 //remove_google_book(NULL);
-?>
-Hello World
+?>Hello World
