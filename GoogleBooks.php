70d6546bd0f2b263e137801e5e0324cb4962f97f
Walter <preparationh67@gmail.com>
2012-04-25 01:55:15 -0500

    Remove Function Complete

--- GoogleBooks.php
+++ GoogleBooks.php
@@ -61,8 +61,40 @@ function find_google_book($pISBN, $pAuthor = array(), $pTitle, $pKeywords = arra
 function remove_google_book($pBookObject)
 {
 	
+	$client = new apiClient();
+	$client->setApplicationName(GB_API_APP_NAME);
+	
+	session_start();
+	//Set cached access token
+	if (isset($_SESSION['gb_api_token']))
+	{
+		$client->setAccessToken($_SESSION['gb_api_token']);
+	}
+
+	//Load key in PKCS 12 format
+	$gb_key = file_get_contents(GB_API_KEY_FILE);
+	$client->setAssertionCredentials(new apiAssertionCredentials(GB_API_SERVICE_ACCOUNT_EMAIL, array('https://www.googleapis.com/auth/books'), $gb_key));
+	$client->setClientId(GB_API_CLIENT_ID);
+
+	$service = new apiBooksService($client);
+	$mylib = $service->mylibrary_bookshelves;
+	//Book code goes here
+//	$shelf2 = $mylib->get(GB_API_BOOKSHELF_UID, array());
+//	echo $shelf2->getVolumeCount();
+	#echo $mylib->addVolume(2, 'HFjLm2BauZ8C', array());
+	#if(!$mylib->addVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array()))
+	$mylib->removeVolume(GB_API_BOOKSHELF_UID, $pBookObject->getVolumeID, array());
+	
+	//Update access token
+	if($client->getAccessToken())
+	{
+		$_SESSION['gb_api_token'] = $client->getAccessToken();
+	}
+
+	return 1;
 }
 
 //add_google_book(NULL);
+//remove_google_book(NULL);
 ?>
 Hello World
